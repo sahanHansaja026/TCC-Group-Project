782c869ea7ac4fdd5aca650587f1dc18ef27f1a3
Sahan Hansaja <165319843+sahanHansaja026@users.noreply.github.com>
2025-08-19 13:16:43 +0530

    home page ui modification

--- mobile/config/ipconfig.js
+++ mobile/config/ipconfig.js
@@ -1,5 +1,5 @@
 // config.js
 
-const API_BASE_URL = "http://192.168.1.2:8000"; // <-- Only change this IP here
+const API_BASE_URL = "http://192.168.1.5:8000"; // <-- Only change this IP here
 
 export default API_BASE_URL;
